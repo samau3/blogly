ed27292f0bbe18b4008b23004c72a6b738f4cbeb
samau3 <samau415@gmail.com>
2021-09-16 12:32:27 -0700

    bug related to Post and User model relationship, commented out to prevent app breaking

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy, func
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
@@ -13,7 +13,7 @@ def connect_db(app):
 class User(db.Model):
     """User Model"""
 
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = db.Column(db.Integer,
                    primary_key=True,
@@ -23,22 +23,21 @@ class User(db.Model):
     last_name = db.Column(db.String(50),
                           nullable=False)
     image_url = db.Column(db.String())
-    posts_list = db.Relationship('Post', backref = 'posts')
+    # posts_list = db.Relationship('Post', backref='posts')
 
 
 class Post(db.Model):
     """Post Model"""
 
-    __tablename__ = 'posts'
+    __tablename__ = "posts"
 
-    id = db.Column(db.Integer,
-                   primary_key=True,
-                   autoincrement=True)
+    post_id = db.Column(db.Integer,
+                        primary_key=True,
+                        autoincrement=True)
     title = db.Column(db.String(50),
                       nullable=False)
     content = db.Column(db.String(),
                         nullable=False)
-    created_at = db.Column(db.DateTime,
-                           default=func.now())
-    user_id = db.Column(db.Text,
-                           db.ForeignKey('users.id'))
+    created_at = db.Column(db.DateTime)
+    # id = db.Column(db.Text,
+    #                db.ForeignKey('users.id'))
