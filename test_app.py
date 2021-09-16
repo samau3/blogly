3f170fa7f7e34994f77b5e59123f02b8dda958e9
samau3 <samau415@gmail.com>
2021-09-16 11:30:54 -0700

    resolved test case bugs related to posting

--- test_app.py
+++ test_app.py
@@ -1,13 +1,38 @@
-from models import User 
+from models import User, db
 from unittest import TestCase
 from app import app
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test_db'
+app.config['SQLALCHEMY_ECHO'] = False
 
 app.config['TESTING'] = True
 
 app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 
+db.drop_all()
+db.create_all()
+
+
 class UserTestCase(TestCase):
     """Test User input routes"""
+
+    def setUp(self):
+        """Add sample pet."""
+
+        User.query.delete()
+
+        user = User(first_name="TestUser", last_name="TestLast", image_url='')
+        db.session.add(user)
+        db.session.commit()
+
+        self.user_id = user.id
+
+    def tearDown(self):
+        """Clean up any fouled transaction."""
+
+        db.session.rollback()
+
 
     def test_home_page(self):
         """Testing redirect to user-list"""
@@ -42,20 +67,21 @@ class UserTestCase(TestCase):
         """Testing value forms of new user"""
         with app.test_client() as client: 
 
-            resp = client.post('/users/new', data = {'first-name': 'Dave', 'last-name': 'Baxter', 'image-url': ''})
-
-        
-            first_name = resp['first-name']
-            last_name = resp['last-name']
-            image_url = resp['image-url']
-
-            #html = resp.get_data(as_text=True)
+            user_data = {"first-name": "TestUser2", "last-name": "TestLast2", "image-url": ''}
+            resp = client.post("/users/new", data=user_data, follow_redirects=True)
+            html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
-            self.assertEqual(type(first_name), str)
-            self.assertEqual(type(last_name), str)
-            self.assertEqual(type(image_url), str)
+            self.assertIn("TestUser2", html)
+
+    def test_user_detail(self):
+        """Testing user profile information page"""
+        with app.test_client() as client:
 
 
-             
+            resp = client.get(f"/users/{self.user_id}")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<h1>TestUser TestLast</h1>', html)
              
